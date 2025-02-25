72ca7ea6bdf0bf3de8cd022ca881cbf3858ce3ac
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2025-02-26 00:38:40 +0900

    ログイン機能追加

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HelloController;
-use App\Http\Controllers\MemberController;
-use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/hello', function () {
-//     return '<h1>HELLO WORLD</h1>';
-// });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::get('/hello', 'HelloController@hello');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/hello', [HelloController::class, 'hello']);
-
-Route::get('/members', [MemberController::class, 'index']);
-
-Route::get('/products', [ProductController::class, 'index']);
+require __DIR__.'/auth.php';
