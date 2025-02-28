4252565d3adf218326514ce490fc967fa7dbc686
fukushima hayao <nu0640042@gmail.con>
2025-03-01 02:33:32 +0900

    かれんだー追加の修正

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,24 +1,6 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
-
+import './calendar.js';
 window.Alpine = Alpine;
-
 Alpine.start();
 
-
-import { Calendar } from "@fullcalendar/core";
-import dayGridPlugin from "@fullcalendar/daygrid"
-import timeGridPlugin from "@fullcalendar/timegrid";
-
-let calendarEl = document.getElementById('calendar');
-let calendar = new Calendar(calendarEl, {
-    plugins: [ dayGridPlugin, timeGridPlugin],
-    initialView: 'dayGridMonth',
-    headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,listWeek'
-    }
-});
-calendar.render();
---
+++ resources/js/calendar.js
@@ -0,0 +1,16 @@
+import { Calendar } from "@fullcalendar/core";
+import dayGridPlugin from "@fullcalendar/daygrid"
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
+let calendarEl = document.getElementById('calendar');
+let calendar = new Calendar(calendarEl, {
+    plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin],
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,listWeek'
+    }
+});
+calendar.render();
