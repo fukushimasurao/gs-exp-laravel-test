64fa4d4cd1fec1076c303425678561c43313c35c
fukushima hayao <nu0640042@gmail.con>
2025-03-01 01:54:08 +0900

    カレンダー追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,20 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+
+import { Calendar } from "@fullcalendar/core";
+import dayGridPlugin from "@fullcalendar/daygrid"
+import timeGridPlugin from "@fullcalendar/timegrid";
+
+let calendarEl = document.getElementById('calendar');
+let calendar = new Calendar(calendarEl, {
+    plugins: [ dayGridPlugin, timeGridPlugin],
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,listWeek'
+    }
+});
+calendar.render();
