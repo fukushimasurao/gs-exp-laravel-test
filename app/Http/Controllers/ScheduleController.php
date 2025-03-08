4c44c91e977a427098987f93a355b467973be8dc
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2025-03-08 18:18:54 +0900

    授業内容コミット

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 use App\Models\Schedule; // ⭐️ScheduleModelを利用するためここで呼び出す。
 use Illuminate\Support\Facades\Auth; // ⭐️Auth::id()を利用するためここで呼び出す。
 
-
 class ScheduleController extends Controller
 {
     public function scheduleAdd(Request $request)
@@ -31,4 +30,82 @@ class ScheduleController extends Controller
 
         return; // viewの返答はいらないので、returnだけ書いて終わり。
     }
+
+    // ⭐️ここから追加！！！
+    public function scheduleGet(Request $request)
+    {
+        // バリデーション
+        $request->validate([
+            'start_date' => 'required|integer',
+            'end_date' => 'required|integer'
+        ]);
+
+        //  -------------- バリデーション終了
+
+        // カレンダー表示期間
+        // JSから送られてきた形式を下記で利用できる用に変形している
+        $start_date = date('Y-m-d', $request->input('start_date') / 1000);
+        $end_date = date('Y-m-d', $request->input('end_date') / 1000);
+
+        // Scheduleから取得処理
+        $schedules = Schedule::query()
+            ->select(
+            // テーブルのカラム名を、FullCalendarの形式に合わせる
+                'start_date as start',
+                'end_date as end',
+                'event_name as title'
+            )
+            // FullCalendarの表示範囲のみ表示
+            ->where('end_date', '>', $start_date)
+            ->where('start_date', '<', $end_date)
+            ->where('user_id', Auth::id()) // ログインユーザーのスケジュールのみ取得
+            ->get();
+
+            // 終日の予定の時間部分は、00:00:00として保存している。
+            // このような00:00:00のものは、時間部分を消して'Y-m-d'の形に変更しする。
+            // FullCalendar側が'Y-m-d'の形式でないと終日の予定として認識できないため。
+            $schedules = $schedules->map(function ($event) {
+            $startDate = new \DateTime($event->start);
+            $endDate = new \DateTime($event->end);
+
+            if ($startDate->format('H:i:s') === '00:00:00' && $endDate->format('H:i:s') === '00:00:00') {
+                $event->start = $startDate->format('Y-m-d');
+                $event->end = $endDate->format('Y-m-d');
+            }
+
+            return $event;
+        });
+
+        return $schedules;
+    }
+
+    // ⭐️ スケジュールデータを取得する
+    public function scheduleList()
+    {
+        // Schedule modelから、user_idが今ログインしている人のIDだけを抽出して、それをstart_date昇順にして取得
+        $schedules = Schedule::where('user_id', Auth::id())->orderBy('start_date', 'asc')->get();
+        return view('schedule-list', compact('schedules'));
+    }
+
+    // show($id)の$idは、URLで渡される'/schedules/{id}'のidの部分です。URLが`/schedules/19`だったら、この引数にも19が渡されます。
+    public function show($id)
+    {
+        // findOrFail()は、データベースからレコードを取得し、もしそのレコードが存在しない場合は404エラー（Not Found）を出してくれる
+        $schedule = Schedule::findOrFail($id);
+        return view('schedule-show', compact('schedule'));
+    }
+
+ // ⭐️追加。
+    public function destroy($id)
+    {
+        // showメソッドと同じ。Scheduleからひとつ抜き出す。
+        $schedule = Schedule::findOrFail($id);
+
+        // delete()で文字通り削除。
+        $schedule->delete();
+
+        // 'schedule-list'にリダイレクト。'schedule-list'はrouteでつけた「あだ名」の部分です。
+        return redirect()->route('schedule-list');
+    }
+
 }
--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -1,47 +1,73 @@
 import { Calendar } from "@fullcalendar/core";
-import dayGridPlugin from "@fullcalendar/daygrid"
+import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
-let calendarEl = document.getElementById('calendar');
-let calendar = new Calendar(calendarEl, {
-    plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin],
-    initialView: 'dayGridMonth',
-    headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,listWeek'
-    },
-    locale: "ja",
-    timeZone: 'Asia/Tokyo',
-    selectable: true,
-    allDaySlot: true,
-    select: function(info) {
-        // alert('selected ' + info.startStr + ' to ' + info.endStr);
-        const eventName = prompt("Enter a Title for the Event");
-        if (eventName) {
+document.addEventListener("DOMContentLoaded", function () {
+    if (window.location.pathname !== "/dashboard") {
+        return;
+    }
+
+    let calendarEl = document.getElementById("calendar");
+    let calendar = new Calendar(calendarEl, {
+        plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin],
+        initialView: "dayGridMonth",
+        headerToolbar: {
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth timeGridWeek",
+        },
+        locale: "ja",
+        timeZone: "Asia/Tokyo",
+        selectable: true,
+        allDaySlot: true,
+        select: function (info) {
+            // alert('selected ' + info.startStr + ' to ' + info.endStr);
+            const eventName = prompt("Enter a Title for the Event");
+            if (eventName) {
+                axios
+                    .post("/schedule-add", {
+                        start_date: info.start.valueOf(),
+                        end_date: info.end.valueOf(),
+                        event_name: eventName,
+                    })
+                    .then(() => {
+                        calendar.addEvent({
+                            title: eventName,
+                            start: info.start,
+                            end: info.end,
+                            // allDay: true,
+                            // borderColor: 'red', // 境界線の色。自由に変えてね
+                            // textColor: 'blue', // テキストの色。自由に変えてね
+                            // backgroundColor: 'yellow', // 背景の色。自由に変えてね
+                        });
+                    })
+                    .catch((error) => {
+                        console.error("Error response:", error.response);
+                        alert("登録に失敗しました");
+                    });
+            }
+        },
+
+        events: function (info, successCallback, failureCallback) {
             axios
-                .post("/schedule-add", {
+                // schedule-getに対して開始日と終了日をpost
+                .post("/schedule-get", {
                     start_date: info.start.valueOf(),
                     end_date: info.end.valueOf(),
-                    event_name: eventName,
                 })
-                .then(() => {
-                    calendar.addEvent({
-                        title: eventName,
-                        start: info.start,
-                        end: info.end,
-                        allDay: true,
-                        // borderColor: 'red', // 境界線の色。自由に変えてね
-                        // textColor: 'blue', // テキストの色。自由に変えてね
-                        // backgroundColor: 'yellow', // 背景の色。自由に変えてね
-                    });
+                .then((response) => {
+                    // 問題なく帰ってきたら、今ブラウザに表示されているイベントを一旦全部消す
+                    calendar.removeAllEvents();
+                    // 取得したデータ（response.data）をFullCalendarに渡す。
+                    // successCallback関数はFullCalendarが用意したもの。これで表示される。
+                    successCallback(response.data);
                 })
-                .catch((error) => {
-                    console.error("Error response:", error.response);
-                    alert("登録に失敗しました");
+                .catch(() => {
+                    alert("取得に失敗しました");
                 });
-        }
-    },
+        },
+
+    });
+    calendar.render();
 });
-calendar.render();
